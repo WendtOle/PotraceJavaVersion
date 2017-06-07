2c8b0fee91091e9b8d5504ea3303e5aa84aa1da4
AndreyDelany <dielanie@freenet.de>
2017-06-07 12:57:43 +0200

    gave testcases better Names

--- test/potrace/auxiliaryTest.java
+++ test/potrace/auxiliaryTest.java
@@ -1,65 +1,61 @@
 package potrace;
 
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by andreydelany on 21/03/2017.
- */
 public class auxiliaryTest {
     @Test
-    public void testMod_positivInput() throws Exception {
+    public void testMethod_Mod_withPositivInput() {
         assertEquals(4,auxiliary.mod(4,9));
         assertEquals(7,auxiliary.mod(57,10));
     }
 
     @Test
-    public void testMod_negativInput() throws Exception {
+    public void testMethod_Mod_withNegativInput() {
         assertEquals(5,auxiliary.mod(-4,9));
         assertEquals(3,auxiliary.mod(-57,10));
     }
 
     @Test
-    public void testSign_postiv() throws Exception {
+    public void testMethod_Sign_withPostivInput() {
         assertEquals(1,auxiliary.sign(4));
         assertEquals(1,auxiliary.sign(4.4));
         assertEquals(1,auxiliary.sign(0.1));
     }
 
     @Test
-    public void testSign_negativ() throws Exception {
+    public void testMethod_Sign_withNegativInput() {
         assertEquals(-1,auxiliary.sign(-4));
         assertEquals(-1,auxiliary.sign(-4.3));
         assertEquals(-1,auxiliary.sign(-0.1));
     }
 
     @Test
-    public void testSign_zero() throws Exception {
+    public void testMethod_Sign_withZeroAsInput() {
         assertEquals(0,auxiliary.sign(0));
         assertEquals(0,auxiliary.sign(0.0));
     }
 
     @Test
-    public void testAbs() throws Exception {
+    public void testMethod_Abs() {
         assertEquals(5637,auxiliary.abs(5637));
         assertEquals(5637,auxiliary.abs(-5637));
     }
 
     @Test
-    public void testFloordiv_pos() throws Exception {
+    public void testMethod_Floordiv_withPositivInput() {
         assertEquals(0,auxiliary.floordiv(5,6));
         assertEquals(1,auxiliary.floordiv(8,5));
     }
 
     @Test
-    public void testFloordiv_neg() throws Exception {
+    public void testMethod_Floordiv_withNegativInput() {
         assertEquals(-1,auxiliary.floordiv(-5,6));
         assertEquals(-2,auxiliary.floordiv(-8,5));
     }
 
     @Test
-    public void testMin() throws Exception {
+    public void testMethod_Min() {
         assertEquals(5,auxiliary.min(5,6));
         assertEquals(5,auxiliary.min(8,5));
         assertEquals(5,auxiliary.min(5,5));
@@ -68,7 +64,7 @@ public class auxiliaryTest {
     }
 
     @Test
-    public void testInterval() throws Exception {
+    public void testMethod_Interval()  {
         dpoint firstPoint = new dpoint(1.2,3.4);
         dpoint secondPoint = new dpoint(3.5,8.1);
         double lampda = 0.5;
