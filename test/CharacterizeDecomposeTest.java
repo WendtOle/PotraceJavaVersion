68f48fadd47c57bc72598b90234f7cb53805de51
AndreyDelany <dielanie@freenet.de>
2017-07-11 17:40:21 +0200

    refactored CharakterizeDecompseTest

---
+++ test/CharacterizeDecomposeTest.java
@@ -0,0 +1,137 @@
+import AdditionalCode.Input.JSONDeEncoder;
+import General.Bitmap;
+import General.DecompositionInterface;
+import General.Param;
+import General.Path;
+import org.json.simple.parser.ParseException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import original.Decompose;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static TestMethods.AssertPathes.assertEqualPathes;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by andreydelany on 08/04/2017.
+ */
+
+@RunWith(Parameterized.class)
+public class CharacterizeDecomposeTest {
+    Path expectedPath;
+    Path actualOriginalPath;
+    Path actualRefactoredPath;
+    Path actualCurrentPathUnderTest, expectedCurrentPathUnderTest;
+    int currentPathPairIndex;
+
+    @Parameterized.Parameters(name = "Testing {index}. Bitmap")
+    public static Collection testData() {
+        return Arrays.asList(getTestParameters("testPictures"));
+    }
+
+    private static Object[][] getTestParameters(String folderNameOfTestPictures) {
+        return getAvailableBitmapPathPairs(folderNameOfTestPictures);
+    }
+
+    private static Object[][] getAvailableBitmapPathPairs(String folderNameOfTestPictures) {
+        File[] bitmapFiles = getAllBitmapFilesForTesting(folderNameOfTestPictures);
+        Object[][] testParameters = new Object[bitmapFiles.length][];
+        for (int i = 0; i < bitmapFiles.length; i++)
+            testParameters[i] = loadBitmapPathPair(bitmapFiles[i]);
+        return testParameters;
+    }
+
+    private static Object[] loadBitmapPathPair(File bitmapFile){
+        Bitmap bitmap = null;
+        Path path = null;
+        try {
+            bitmap = JSONDeEncoder.readBitmapFromJSon(bitmapFile);
+            path = JSONDeEncoder.readTestDataFromJSon(bitmapFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return new Object[]{bitmap,path};
+    }
+
+    private static File[] getAllBitmapFilesForTesting(String folderNameOfTestPictures) {
+        return new File(folderNameOfTestPictures).listFiles((dir, name) -> {
+                    return name.toLowerCase().endsWith(".json");
+                }
+        );
+    }
+
+    public CharacterizeDecomposeTest(Bitmap bitmap, Path expectedPath) {
+        this.expectedPath = expectedPath;
+        setActualOriginalPath(bitmap);
+        setActualRefactoredPath(bitmap);
+    }
+
+    private void setActualOriginalPath(Bitmap bitmap) {
+        DecompositionInterface originalDecomposer = new Decompose();
+        this.actualOriginalPath = originalDecomposer.getPathList(bitmap,new Param());
+    }
+
+    private void setActualRefactoredPath(Bitmap bitmap) {
+        DecompositionInterface originalDecomposer = new Decompose();
+        this.actualRefactoredPath = originalDecomposer.getPathList(bitmap,new Param());
+    }
+
+    @Test
+    public void testOriginalDecomposer(){
+        checkThatAllPathsAreTheSame(actualOriginalPath);
+    }
+
+    @Test
+    public void testRefactoredDecomposer(){
+        checkThatAllPathsAreTheSame(actualRefactoredPath);
+    }
+
+    private void checkThatAllPathsAreTheSame(Path actualPath) {
+        testNumberOfPaths(actualPath);
+        initializeFirstTestPairOfPaths(actualPath);
+        testAllTestPathPairs();
+    }
+
+    private void testNumberOfPaths(Path actualPath) {
+        assertEquals("Number of Pathes", countPaths(expectedPath), countPaths(actualPath));
+    }
+
+    private void initializeFirstTestPairOfPaths(Path actualPath) {
+        expectedCurrentPathUnderTest = expectedPath;
+        actualCurrentPathUnderTest = actualPath;
+        currentPathPairIndex = 0;
+    }
+
+    private void testAllTestPathPairs() {
+        do {
+            compareCurrentTestPathPair();
+            setNextPairOfPathsToCompare();
+        } while (expectedCurrentPathUnderTest.next != null);
+    }
+
+    private void compareCurrentTestPathPair() {
+        assertEqualPathes(currentPathPairIndex,expectedCurrentPathUnderTest,actualCurrentPathUnderTest);
+    }
+
+    private void setNextPairOfPathsToCompare() {
+        expectedCurrentPathUnderTest = expectedCurrentPathUnderTest.next;
+        actualCurrentPathUnderTest = actualCurrentPathUnderTest.next;
+    }
+
+    private int countPaths(Path path) {
+        int actualAmountOfPathes = 1;
+        Path currentPath = path;
+        while (currentPath.next != null) {
+            actualAmountOfPathes ++;
+            currentPath = currentPath.next;
+        }
+        return actualAmountOfPathes;
+    }
+}
