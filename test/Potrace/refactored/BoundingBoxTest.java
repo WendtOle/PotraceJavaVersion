8d2b80a63fede1fc68e54c0cdf6e794f4c872234
AndreyDelany <dielanie@freenet.de>
2017-07-12 10:18:37 +0200

    refactored BoundingBoxPath

--- test/Potrace/refactored/BoundingBoxTest.java
+++ test/Potrace/refactored/BoundingBoxTest.java
@@ -1,7 +1,7 @@
 package Potrace.refactored;
 
-import Potrace.General.Bitmap;
 import Potrace.General.Path;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.awt.*;
@@ -11,19 +11,23 @@ import static org.junit.Assert.assertEquals;
  * Created by andreydelany on 24.06.17.
  */
 public class BoundingBoxTest {
+
+    Path dummyPath = new Path();
+
+    @Before
+    public void setPath(){
+        Point[] pointsOfPath = new Point[]{new Point(1,2),new Point(1,1),new Point(2,1),new Point(2,2)};
+        dummyPath.priv.pt = pointsOfPath;
+        dummyPath.priv.len = 4;
+    }
+
     @Test
-    public void test_setbbox_path() {
-        Bitmap testBitmap = new Bitmap(3,3);
-        BitmapHandlerInterface manipulator = new BitmapHandler(testBitmap);
-        manipulator.setPixel(new Point(1,1));
+    public void testInitializingBoundingBoxWithPath() {
+        BoundingBox box = new BoundingBox(dummyPath);
 
-        FindPath findPath = new FindPath(testBitmap,new Point(1,1),43, TurnPolicyEnum.MINORITY);
-        Path path = findPath.getPath();
-
-        BoundingBox box = new BoundingBox(path);
-        assertEquals(1,box.x0);
-        assertEquals(2,box.x1);
-        assertEquals(1,box.y0);
-        assertEquals(2,box.y1);
+        assertEquals("min X",1,box.x0);
+        assertEquals("max X",2,box.x1);
+        assertEquals("min Y",1,box.y0);
+        assertEquals("max Y",2,box.y1);
     }
 }
